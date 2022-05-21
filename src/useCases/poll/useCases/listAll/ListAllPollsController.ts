8fa004216df202d7dae2cce215c41479d19db08a
Leandro Lopes <leandro-lopes46@hotmail.com>
2022-05-21 01:02:33 -0300

    style: add correct name on ListAllPollsController

--- src/useCases/poll/useCases/listAll/ListAllPollsController.ts
+++ src/useCases/poll/useCases/listAll/ListAllPollsController.ts
@@ -7,9 +7,9 @@ import { ListAllPollsUseCase } from './ListAllPollsUseCase'
 export class ListAllPollsController {
   async handle(req: Request, res: Response) {
     const pollsRepository = new PollRepository()
-    const createPollUseCase = new ListAllPollsUseCase(pollsRepository)
+    const listAllPollsUseCase = new ListAllPollsUseCase(pollsRepository)
 
-    const polls = await createPollUseCase.execute()
+    const polls = await listAllPollsUseCase.execute()
 
     return res.status(200).json(polls)
   }
