cd0f6305fb28faf27ddae915debcaf07fc200150
Leandro Lopes <leandro-lopes46@hotmail.com>
2022-05-20 23:16:57 -0300

    feat: add error handler

--- src/server.ts
+++ src/server.ts
@@ -1,13 +1,29 @@
-import express from 'express'
+import express, { NextFunction, Request, Response } from 'express'
 import cors from 'cors'
 
+import 'express-async-errors'
+
 import { routes } from './routes'
+import { AppError } from './error/AppError'
 
 const app = express()
 
 app.use(cors())
 app.use(express.json())
 app.use(routes)
+
+app.use((error: Error, req: Request, res: Response, next: NextFunction) => {
+  if (error instanceof AppError) {
+    return res.status(error.statusCode).json({
+      message: error.message,
+    })
+  }
+
+  return res.status(500).json({
+    status: 'error',
+    message: `Internal server error - ${error.message}`,
+  })
+})
 
 app.listen(process.env.PORT || 3333, () => {
   console.log('Server is running')
